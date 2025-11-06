fd491d1bec60fc23965c1a938eb9564faa9118c5
Celestin <celestin.meunier@gmail.com>
2025-11-06 21:21:18 +0100

    fixed text

--- src/routes/curators/+page.svelte
+++ src/routes/curators/+page.svelte
@@ -3,20 +3,22 @@
 </script>
 
 <div class="flex w-screen items-center bg-zinc-900 p-4">
-	<div class="flex max-w-2xl flex-col gap-4 text-zinc-100">
+	<div class="flex max-w-2xl flex-col gap-4 text-zinc-400">
 		<div class="mb-4 w-32 fill-zinc-100">
 			<Logo />
 		</div>
 
-		<h1 class="font-bold">Public Shorts: Video Festival in Berlin 2026, Curated by <i>you?</i></h1>
+		<h1 class="font-bold text-zinc-100">
+			Public Shorts: Video Festival in Berlin 2026, Curated by <i>you?</i>
+		</h1>
 
-		<p class="text-zinc-300">
+		<p class="">
 			Hi, if you’ve received this link, it’s because you’re invited to take part in the curation of
 			the upcoming <i>Public Shorts</i> video festival.
 		</p>
 
-		<h2 class="font-bold">Public Shorts?</h2>
-		<p class="text-zinc-300">
+		<h2 class="font-bold text-zinc-100">Public Shorts?</h2>
+		<p class="">
 			We are putting together a video festival, which will stream 24/7 on screens inside the glass
 			window of <a href="https://kanape.studio">kanapé</a>, which means that anyone could come and
 			watch the shorts during the festival period
@@ -26,8 +28,8 @@
 			On top of this, we will have 3 screening sessions with the different shorts, with Q&A,
 			debates, etc.
 		</p>
-		<h2 class="font-bold">What does that entail?</h2>
-		<p class="text-zinc-300">
+		<h2 class="font-bold text-zinc-100">What does that entail?</h2>
+		<p class="">
 			You’ll review submitted works, take notes (Phase 1), and join collective discussions (Phase 2)
 			to shape the final selection. We're excpecting a lot of submissions so we are trying to gather
 			a diverse and numerous group of curators so it remains manageable for everyone. We planned two
@@ -39,8 +41,8 @@
 			It would be nice to particpate to a least 1 session.
 		</p>
 
-		<h2 class="font-bold">Timeline and key dates</h2>
-		<ul class="list-inside list-disc text-zinc-300">
+		<h2 class="font-bold text-zinc-100">Timeline and key dates</h2>
+		<ul class="list-inside list-disc">
 			<li><b>Open Call:</b> 12 Dec 2025 – 10 Jan 2025</li>
 			<li class="text-green-600"><b>Curation Phase 1 (Individual viewing):</b> 19 Jan – 31 Jan</li>
 			<li class="text-green-600"><b>Curation Phase 2 (Collective curation):</b> 1 Feb – 15 Feb</li>
@@ -51,15 +53,30 @@
 			<li><b>Festival second screening session:</b> 6 Mar</li>
 			<li><b>Festival closing and last screening session:</b> 13 Mar</li>
 		</ul>
-		<h2 class="font-bold">Is this paid?</h2>
+		<h2 class="font-bold text-zinc-100">Is this paid?</h2>
 		<p>
 			No. We are all doing this together as volunteers, we are searching for fundings and sponsors
 			but are not counting on it. 🤷 Maybe next year!
 		</p>
-		<h2 class="font-bold">Next step</h2>
-		<p class="text-zinc-300">
+		<h2 class="font-bold text-zinc-100">Next step</h2>
+		<p class="">
 			Please confirm your participation with the person who sent you this link. Thank you so much
 			for your help :)
 		</p>
 	</div>
 </div>
+
+<style>
+	p,
+	li {
+		color: var (--zinc-400);
+	}
+	h1,
+	h2 {
+		color: var (--zinc-100);
+	}
+	a {
+		color: var (--zinc-100);
+		text-decoration: underline;
+	}
+</style>

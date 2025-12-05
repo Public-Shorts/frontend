5b554373a56bf87b95d7e257db2cb92466f98932
manegame <contact@manusnijhoff.nl>
2025-12-05 11:54:17 +0100

    fix: submit page unlock

--- src/routes/(protected)/submit/+layout.svelte
+++ src/routes/(protected)/submit/+layout.svelte
@@ -13,8 +13,8 @@
 {#if unlocked}
   {@render children()}
 {:else}
-<div class="relative">
-  <button class="" onclick={progress}>
+<div class="absolute w-full h-20 bottom-0 left-0 flex justify-center items-center">
+  <button class:text-accent-500={i > 6} class:font-bold={i > 6} onclick={progress}>
     Datenschutz
   </button>
 </div>
